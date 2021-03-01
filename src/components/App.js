b756c3eb1d3eb5f34770b7e2ba4608863d410151
Josh Frank <eurosewusa@gmail.com>
2021-02-28 22:26:54 -0500

    commit tree a79819ea339d893f6390cdd3662e795f258f3806

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import APIUrl from "./APIUrl";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+
+  const [ pizzas, setPizzas ] = useState( [] );
+  const [ currentPizza, setCurrentPizza ] = useState( { topping: "", size: "", vegetarian: true } );
+  const [ pizzaFormState, setPizzaFormState ] = useState( { topping: "", size: "", vegetarian: true } );
+
+  useEffect( () => {
+    fetch( APIUrl ).then( response => response.json() ).then( setPizzas );
+  }, [] );
+
+  // console.log( pizzaFormState );
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm
+        allPizzas={ pizzas }
+        setPizzas={ setPizzas }
+        currentPizza={ currentPizza }
+        pizzaFormState={ pizzaFormState }
+        setPizzaFormState={ setPizzaFormState }
+      />
+      <PizzaList pizzas={ pizzas } setCurrentPizza={ setCurrentPizza } setPizzaFormState={ setPizzaFormState }/>
     </>
   );
+
 }
 
 export default App;
--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList( { pizzas, setCurrentPizza, setPizzaFormState } ) {
   return (
     <table className="table table-striped">
       <thead>
@@ -13,9 +13,7 @@ function PizzaList() {
         </tr>
       </thead>
       <tbody>
-        {
-          //render Pizza here
-        }
+        { pizzas.map( pizza => <Pizza key={ pizza.id } pizza={ pizza } setCurrentPizza={ setCurrentPizza } setPizzaFormState={ setPizzaFormState }/> ) }
       </tbody>
     </table>
   );
